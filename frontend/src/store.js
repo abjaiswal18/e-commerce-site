171d6a6b0e3beb14be983f992a0b63e9d8ef226d
Abhishek <abjaiswal1997@gmail.com>
2020-10-19 16:58:59 +0530

    Build CartScreen With redux along with delete button

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 import  {createStore, combineReducers, compose, applyMiddleware} from "redux";
 import {productDetailsReducer, productListReducer} from "./reducers/productReducers";
 import thunk from "redux-thunk";
+import { CartReducer } from "./reducers/CartReducers";
+
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: CartReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
